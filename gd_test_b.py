e4e1c8bea065087998be54536861233f847f4310
brandonalston <bca3@rice.edu>
2024-03-25 18:02:24 -0500

    mip bias, margin, gd model testing

--- gd_test_b.py
+++ gd_test_b.py
@@ -8,7 +8,7 @@ learning_rate = [1e-3,1e-4,1e-5,1e-6]
 tf_seed = [66,56,16]
 
 # GD
-for ec in examples_per_class:
+for ec in [2,8]:
     for lr in learning_rate:
         for tfs in tf_seed:
-            model_runs.gd_run(["-h", 0, "-s", f's_{15}', "-r", lr, "-c", ec, "-t", time_limit, "-i", tfs, "-f", 'bnn_runs.csv'])
+            model_runs.gd_run(["-h", 0, "-s", f's_{15}', "-r", lr, "-c", 2, "-t", time_limit, "-i", tfs, "-f", 'bnn_runs.csv'])
