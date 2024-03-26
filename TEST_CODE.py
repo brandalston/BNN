73350c37e2880a1498debdeb06243d70fa32a2f9
brandonalston <bca3@rice.edu>
2024-03-25 22:18:10 -0500

    mip bias, margin, gd model testing

--- TEST_CODE.py
+++ TEST_CODE.py
@@ -1,4 +1,4 @@
-import model_runs, dataload, UTILS, warnings
+import model_runs, dataload, UTILS, warnings, csv
 warnings.filterwarnings("ignore")
 
 """
@@ -9,12 +9,31 @@ print(len(images), images.shape)
 for i in range(5):
     print(images[i],'\n', type(images[i]))
 """
-n_hidden_layers = [0, 1, 2]
-examples_per_class = [1,5,10]
+n_hidden_layers = [1, 2]
+examples_per_class = [1,2,5,8,10]
 rand_states = [0, 15, 42, 89, 138]
 time_limit = 60
 learning_rate = [1e-3,1e-4,1e-5,1e-6]
 tf_seed = [66,56,16]
 
 # model_runs.gd_run(["-h", 0, "-s", f's_{0}', "-r", 1e-3, "-c", 2, "-t", 2, "-i", 42, "-f", 'test_dump.csv'])
-model_runs.mip_run(["-o", 'margin-indicator', "-h", 0, "-c", 5, "-t", 10, "-s", f's_{138}', "-e", 0.0001, "-z", 1, "-f", 'test_dump.csv'])
+# model_runs.mip_run(["-o", 'margin-indicator', "-h", 0, "-c", 5, "-t", 10, "-s", f's_{138}', "-e", 0.0001, "-z", 1, "-f", 'test_dump.csv'])
+
+# MIP
+obj_func = ['bias-indicator', 'margin-indicator']#, 'bias', 'margin']
+model_eps = 0.001
+for obj in ['bias-indicator','margin-indicator']:
+    for seed in rand_states:
+        for nhl in n_hidden_layers:
+            for ec in examples_per_class:
+                try:
+                    model_runs.mip_run(["-o", obj, "-h", nhl, "-c", ec, "-t", time_limit,
+                                        "-s", f's_{seed}', "-e", 0.001, "-z", 0, "-f", 'test_dump.csv'])
+                except:
+                    print(f'Obj: {obj}, HL: {nhl}, EC: {ec} INFEASIBLE!!')
+                    with open('results_files/test_dump.csv', mode='a') as results:
+                        results_writer = csv.writer(results, delimiter=',', quotechar='"')
+                        results_writer.writerow([
+                            0, 10*ec, 'N/A', 'N/A', 'N/A', nhl, obj_func,
+                            'N/A', 'N/A', time_limit, 'N/A', 'N/A', 'N/A'])
+                        results.close()
