40c5891ac7633758c0a1ee639d7c2d29dc06b722
brandonalston <bca3@rice.edu>
2024-03-27 19:12:35 -0500

    mip bias, margin, gd model testing

--- margin_test_e.py
+++ margin_test_e.py
@@ -8,14 +8,13 @@ time_limit = 30
 learning_rate = [1e-3, 1e-4, 1e-5, 1e-6]
 tf_seed = [66, 56, 16]
 
-model_runs.mip_run(["-o", 'margin-indicator', "-h", 0, "-c", 2, "-t", time_limit,
-                    "-s", f's_{138}', "-e", 0.001, "-z", 0, "-f", 'bnn_runs.csv'])
+# model_runs.mip_run(["-o", 'margin-indicator', "-h", 0, "-c", 2, "-t", time_limit,
+#                    "-s", f's_{0}', "-e", 0.001, "-z", 0, "-f", 'bnn_runs.csv'])
 
 
-"""# MIP
+# MIP
 obj_func = ['bias-indicator', 'margin-indicator', 'bias', 'margin']
 model_eps = 0.001
 for ec in examples_per_class:
     model_runs.mip_run(["-o", 'margin-indicator', "-h", 0, "-c", ec, "-t", time_limit,
-                        "-s", f's_{0}', "-e", 0.001, "-z", 0, "-f", 'bnn_runs.csv'])
-"""
+                        "-s", f's_{138}', "-e", 0.001, "-z", 0, "-f", 'bnn_runs.csv'])
